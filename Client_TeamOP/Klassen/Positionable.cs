53ade814fb4b469174ab927ca0927914a8db7439
JanetRahn <JanetRahn@msn.com>
2014-10-29 15:07:19 +0100

    hub==doof Positionable

---
+++ Client_TeamOP/Klassen/Positionable.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client_TeamOP.Klassen
+{
+    class Positionable
+    {
+        private int id;
+        private int points;
+        private int x;
+        private int y;
+        private String description;
+        private String type;
+        private bool busy;
+        private bool autopilot;
+
+
+       public Positionable()
+        {
+
+        }
+
+        public void autopilot(bool status)
+        {
+
+        }
+
+        private void setY(int y)
+        {
+            this.y = y; 
+        }
+
+        private void setX(int x)
+        {
+            this.x = x;
+        }
+
+        public int getX()
+        {
+            return x;
+        }
+
+        public int getY()
+        {
+            return y;
+        }
+
+        public String getDescription()
+        {
+            return description;
+        }
+
+        private void setDescription(String description)
+        {
+            this.description = description;
+        }
+
+        public int getID()
+        {
+            return id;
+        }
+
+        public int getPoints()
+        {
+            return points;
+        }
+
+        public String getType()
+        {
+            return type;
+        }
+
+        public bool getBusy()
+        {
+            return busy;
+        }
+       
+
+    }
+}
