2f0148ac496a012bfcf009a126628f0e557d44cf
kaajalchai <christina.papadopoulos@student.reutlingen-university.de>
2014-10-30 13:27:33 +0100

    Map/Field

---
+++ Client_TeamOP/Klassen/Field.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client_TeamOP.Klassen
+{
+    class Field
+    {
+        private Boolean forest;
+        private Boolean unwalkable;
+        private Boolean water;
+        private Boolean huntable;
+        private Positionable positionable;
+
+        public Field()
+        {
+
+        }
+        public Boolean getForest()
+        {
+            return forest;
+        }
+        public Boolean getUnwalkable()
+        {
+            return unwalkable;
+        }
+        public Boolean getWater()
+        {
+            return water;
+        }
+        public Boolean getHuntable()
+        {
+            return huntable;
+        }
+        public void setPositionable(Positionable pl)
+        {
+            this.positionable = pl;
+        }
+        public void removePositionable(Positionable pl)
+        {
+
+        }
+    }
+}
---
+++ Client_TeamOP/Klassen/Map.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client_TeamOP.Klassen
+{
+    class Map
+    {
+        private Field[] field;
+        private int width, length;
+
+        public Map()
+        {
+
+        }
+        public int[] getField(int x, int y)
+        {
+            int[] array=null;
+            return array;
+        }
+        public int getWidth(){
+            
+            return width;
+        }
+        public int getLength()
+        {
+            return length;
+        }
+
+    }
+}
