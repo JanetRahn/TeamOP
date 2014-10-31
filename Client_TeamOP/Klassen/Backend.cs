f7ccc65d7825e34bcacaef0c17151241733a0169
kaajalchai <christina.papadopoulos@student.reutlingen-university.de>
2014-10-31 08:44:51 +0100

    Upload Backend
    Bug

--- Client_TeamOP/Klassen/Backend.cs
+++ Client_TeamOP/Klassen/Backend.cs
@@ -10,28 +10,125 @@ namespace Client_TeamOP
     public class Backend
     {
         private Connector connector;
+        private Parser parser;
         private GUI gui;
-        private List<Object> positionable;
-        public bool sendToConnector(String message)
+        private List<Positionable> positionable_Human;
+        private List<Positionable> positionable_Dragon;
+        private Map map;
+        private MiniGame currentGame;
+        private Boolean isMiniGame;
+        private List<String> log;
+
+        public bool sendCommand(String message)
         {
             Contract.Requires(message != null);
             Contract.Invariant(connector != null);
             return false;
         }
 
-        public bool storeEnemy(Object positionable)
+        public bool storeHuman(Positionable positionable)
         {
-            Contract.Requires(this.positionable != null);
+            Contract.Requires(this.positionable_Human != null);
             Contract.Requires(positionable != null);
             return false;
         }
 
-        public void deleteEnemey(Object positionable)
+        public void deleteHuman(Positionable positionable)
         {
-            Contract.Requires(this.positionable != null);
+            Contract.Requires(this.positionable_Human != null);
             Contract.Requires(positionable != null);
         }
 
+        public List<Positionable> getPositionableHumans()
+        {
 
+        }
+
+        public bool storeDragon(Positionable positionable)
+        {
+            Contract.Requires(this.positionable_Dragon != null);
+            Contract.Requires(positionable != null);
+            return false;
+        }
+
+        public void deleteDragon(Positionable positionable)
+        {
+            Contract.Requires(this.positionable_Dragon != null);
+            Contract.Requires(positionable != null);
+        }
+
+        public List<Positionable> getPositionableDragon()
+        {
+
+        }
+
+        public Boolean moveUp()
+        {
+            return true;
+        }
+
+        public Boolean moveDown()
+        {
+            return true;
+        }
+
+        public Boolean moveRight()
+        {
+            return true;
+        }
+
+        public Boolean moveLeft()
+        {
+            return true;
+        }
+
+        public void challengePlayer()
+        {
+
+        }
+
+        public void rename()
+        {
+
+        }
+
+        public void connectServer()
+        {
+
+        }
+
+        public Boolean isConnected()
+        {
+            return false;
+        }
+
+        public void disconnectServer()
+        {
+
+        }
+
+        public String getCurrentMiniGame()
+        {
+            return "";
+        }
+
+        public Map getMap()
+        {
+            return map;
+        }
+
+        public void interactMiniGame(String action)
+        {
+        }
+
+        public Boolean isMiniGame()
+        {
+            return false;
+        }
+
+        public List<String> getLog()
+        {
+            return log;
+        }
     }
 }
