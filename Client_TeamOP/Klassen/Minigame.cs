4ecdef1e47024f587a31a40a979f7d80ee780672
TimSchraml <timschraml@web.de>
2014-10-31 08:23:51 +0100

    Minigame + Thread + Map/Field eingebunden
    Nachträglich hochgeladen

---
+++ Client_TeamOP/Klassen/Minigame.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client_TeamOP.Klassen
+{
+    class Minigame
+    {
+        int roundCounter;
+        int points = 0;
+
+        public void interaction()
+        {
+
+        }
+    }
+}
