63ff64540ddfad2c041b259cb9b07ddbc72d85fc
soure123 <steven_x3@live.de>
2014-11-05 23:19:53 +0100

    was ergänzt bei minigame

--- Client_TeamOP/Klassen/Minigame.cs
+++ Client_TeamOP/Klassen/Minigame.cs
@@ -8,12 +8,37 @@ namespace Client_TeamOP.Klassen
 {
     class Minigame
     {
-        int roundCounter;
-        int points = 0;
+        private int roundCounter;
+        private int points = 0;
+        private String gameName;
+        private String[] interactions;
 
-        public void interaction()
+        public void interaction(String gameName)
         {
+            this.gameName = gameName;
+            roundCounter = 0;
+            points = 0;
+            interactions = new String[3];
+        }
 
+        public int getRound()
+        {
+            return roundCounter;
+        }
+
+        public int getPoints()
+        {
+            return points;
+        }
+
+        public String getGameName()
+        {
+            return gameName;
+        }
+
+        public String getInteraction(int x)
+        {
+            return interactions[x];
         }
     }
 }
